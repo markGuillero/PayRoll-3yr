056f4e0b4e7c367075a3657968f6e7cb651b1cff
Mark Guillero <johnmarkguillero1@gmail.com>
2023-03-26 21:34:44 +0800

    Update TestEmail.php

--- Payroll-homemade/TestEmail.php
+++ Payroll-homemade/TestEmail.php
@@ -15,7 +15,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'crossmode75@gmail.com';                     //SMTP username
-    $mail->Password   = 'padktczxsrcsfajo';                               //SMTP password
+    $mail->Password   = '#';                               //SMTP password
     $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
